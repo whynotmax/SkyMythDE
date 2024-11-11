f8871cf2fe9b2ba16e3951ee1aa3b7507de3b364
KeineSecrets <miethe.max@web.de>
2024-11-11 14:55:08 +0100

    Initial commit

---
+++ src/main/java/de/skymyth/SkyMythPlugin.java
@@ -0,0 +1,17 @@
+package de.skymyth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyMythPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
