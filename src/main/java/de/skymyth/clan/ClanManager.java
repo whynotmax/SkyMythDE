53b7bbcbe3c8cb1336c9f42bbbd0b5c747ec4e1c
pesoa <sebastianf0@web.de>
2024-11-13 15:08:10 +0100

    Began Clan

--- src/main/java/de/skymyth/clan/ClanManager.java
+++ src/main/java/de/skymyth/clan/ClanManager.java
@@ -5,37 +5,59 @@ import de.skymyth.clan.model.Clan;
 import de.skymyth.clan.repository.ClanRepository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ClanManager {
 
     SkyMythPlugin plugin;
     ClanRepository repository;
+    Map<String, Clan> clanMap;
 
     public ClanManager(SkyMythPlugin plugin) {
         this.plugin = plugin;
         this.repository = this.plugin.getMongoManager().create(ClanRepository.class);
+        this.clanMap = this.repository.findAll().stream().collect(Collectors.toMap(Clan::getName, clan -> clan));
     }
 
     public void createClan(UUID uuid, String clanName) {
         Clan clan = new Clan();
-        clan.setClanName(clanName);
-        clan.setClanLeader(uuid);
+        clan.setName(clanName);
+        clan.setLeader(uuid);
         clan.setMaxMembers(10);
         clan.setMembers(new ArrayList<>());
         this.repository.save(clan);
+        this.clanMap.put(clanName, clan);
     }
 
-    public Clan getClanByName(String clanName) {
-        return null;
+    public void deleteClan(String clanName) {
+        this.repository.delete(this.clanMap.get(clanName));
+        this.clanMap.remove(clanName);
     }
 
-    public Clan getClanByLeader(UUID uuid) {
-        return null;
+    public void deleteClan(Clan clan) {
+        this.repository.delete(clan);
+        this.clanMap.remove(clan.getName());
     }
+
 
     public boolean existsClan(String clanName) {
-        return this.repository.findFirstById(clanName) != null;
+        return this.clanMap.get(clanName) != null;
+    }
+
+    public Clan getClan(UUID uuid) {
+        for (Clan value : clanMap.values()) {
+            if(value.getLeader().equals(uuid) || value.getMembers().contains(uuid)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public boolean isInClan(UUID uuid) {
+        return this.getClan(uuid) != null;
     }
 
 
--- src/main/java/de/skymyth/clan/repository/ClanRepository.java
+++ src/main/java/de/skymyth/clan/repository/ClanRepository.java
@@ -1,12 +1,15 @@
 package de.skymyth.clan.repository;
 
 import de.skymyth.clan.model.Clan;
+import eu.koboo.en2do.repository.Collection;
 import eu.koboo.en2do.repository.Repository;
 
 import java.util.UUID;
 
+@Collection("clans")
 public interface ClanRepository extends Repository<Clan, String> {
 
     Clan findFirstByLeader(UUID leader);
+
 
 }
