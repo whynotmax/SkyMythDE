534e075c635d922415f90be725922e43978627cd
KeineSecrets <miethe.max@web.de>
2024-11-13 14:27:06 +0100

    Fixed ClanRepository

--- src/main/java/de/skymyth/clan/repository/ClanRepository.java
+++ src/main/java/de/skymyth/clan/repository/ClanRepository.java
@@ -3,7 +3,10 @@ package de.skymyth.clan.repository;
 import de.skymyth.clan.model.Clan;
 import eu.koboo.en2do.repository.Repository;
 
+import java.util.UUID;
+
 public interface ClanRepository extends Repository<Clan, String> {
 
+    Clan findFirstByLeader(UUID leader);
 
 }
