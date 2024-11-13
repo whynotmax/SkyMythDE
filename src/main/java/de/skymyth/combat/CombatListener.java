b97330e86afab276e6ec1e1e9ea296d30eddf8e2
KeineSecrets <miethe.max@web.de>
2024-11-13 13:11:50 +0100

    Added CombatListener and LogoutListener

---
+++ src/main/java/de/skymyth/combat/CombatListener.java
@@ -0,0 +1,48 @@
+package de.skymyth.combat;
+
+import de.skymyth.SkyMythPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public record CombatListener(SkyMythPlugin plugin) implements Listener {
+
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent e) {
+        if((e.getEntity() instanceof Player player) && (e.getDamager() instanceof Player target)) {
+            String world = player.getLocation().getWorld().getName();
+
+            if(!world.equals("Spawn")) {
+                if (!LogoutListener.combat.contains(player)) {
+                    if (!LogoutListener.combat.contains(target)) {
+                        player.sendMessage(SkyMythPlugin.PREFIX + "§7Du bist nun im Kampf§8.");
+                        target.sendMessage(SkyMythPlugin.PREFIX + "§7Du bist nun im Kampf§8.");
+                        LogoutListener.combat.add(player);
+                        LogoutListener.combat.add(target);
+                    }
+                }
+            } else {
+                player.sendMessage(SkyMythPlugin.PREFIX + "§cAm Spawn ist PvP deaktiviert§8.");
+                e.setCancelled(true);
+            }
+
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if(LogoutListener.combat.contains(player)) {
+                        if(LogoutListener.combat.contains(target)) {
+                            LogoutListener.combat.remove(player);
+                            LogoutListener.combat.remove(target);
+                            player.sendMessage(SkyMythPlugin.PREFIX + "§7Du bist nun nicht mehr im Kampf§8.");
+                            target.sendMessage(SkyMythPlugin.PREFIX + "§7Du bist nun nicht mehr im Kampf§8.");
+                        }
+                    }
+                }
+            }, 150L);
+        }
+    }
+}
