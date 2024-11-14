d414c84b8180886aa40784e0f59c583a76494dd6
KeineSecrets <miethe.max@web.de>
2024-11-14 12:49:19 +0100

    deleted crate package

--- src/main/java/de/skymyth/crate/repository/CrateRepository.java
+++
@@ -1,9 +0,0 @@
-package de.skymyth.crate.repository;
-
-import de.skymyth.crate.model.Crate;
-import eu.koboo.en2do.repository.Collection;
-import eu.koboo.en2do.repository.Repository;
-
-@Collection("crates")
-public interface CrateRepository extends Repository<Crate, String> {
-}
