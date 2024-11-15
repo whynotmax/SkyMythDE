3e3284eaa107acbbc72d4d40068922499f61c16d
pesoa <sebastianf0@web.de>
2024-11-15 23:02:42 +0100

    main

--- src/main/java/de/skymyth/commands/impl/SpawnCommand.java
+++ src/main/java/de/skymyth/commands/impl/SpawnCommand.java
@@ -32,42 +32,50 @@ public class SpawnCommand extends MythCommand {
             return;
         }
 
-        player.sendMessage(SkyMythPlugin.PREFIX + "§7Du wirst in §e3 Sekunden§7 zum Spawn teleportiert.");
 
-        Location initialLocation = player.getLocation();
+        if(!player.hasPermission("myth.team")) {
+            player.sendMessage(SkyMythPlugin.PREFIX + "§7Du wirst in §e3 Sekunden§7 zum Spawn teleportiert.");
 
-        BukkitRunnable task = new BukkitRunnable() {
-            private int ticks = 0;
+            Location initialLocation = player.getLocation();
 
-            @Override
-            public void run() {
-                if (!player.getLocation().getBlock().equals(initialLocation.getBlock())) {
-                    player.sendMessage(SkyMythPlugin.PREFIX + "§cDer Vorgang wurde abgebrochen, da du dich bewegt hast.");
-                    teleportTasks.remove(player);
-                    this.cancel();
-                    return;
+            BukkitRunnable task = new BukkitRunnable() {
+                private int ticks = 0;
+
+                @Override
+                public void run() {
+                    if (!player.getLocation().getBlock().equals(initialLocation.getBlock())) {
+                        player.sendMessage(SkyMythPlugin.PREFIX + "§cDer Vorgang wurde abgebrochen, da du dich bewegt hast.");
+                        teleportTasks.remove(player);
+                        this.cancel();
+                        return;
+                    }
+
+                    int secondsLeft = 3 - (ticks / 20);
+
+                    if (ticks % 20 == 0) {
+                        player.playSound(player.getLocation(), Sound.LAVA_POP, 1.0f, 1.0f);
+                        TitleUtil.sendTitle(player, 0, 40, 20, "§e§l" + secondsLeft, "§8× §7Du wirst teleportiert §8×");
+
+                    }
+
+                    ticks++;
+
+                    if (ticks >= 60) {
+                        teleportTasks.remove(player);
+                        player.teleport(plugin.getLocationManager().getPosition("spawn").getLocation());
+                        TitleUtil.sendTitle(player, 0, 40, 20, "§a§lSpawn", "§8× §7Du wurdest teleportiert §8×");
+                        player.playSound(player.getLocation(), Sound.ENDERDRAGON_WINGS, 1.0f, 1.0f);
+                        this.cancel();
+                    }
                 }
+            };
 
-                int secondsLeft = 3 - (ticks / 20);
-
-                if (ticks % 20 == 0) {
-                    player.playSound(player.getLocation(), Sound.LAVA_POP, 1.0f, 1.0f);
-                    TitleUtil.sendTitle(player, 0, 25, 20, "§e" + secondsLeft,"§7§oTeleportiere...");
-                }
-
-                ticks++;
-
-                if (ticks >= 60) {
-                    teleportTasks.remove(player);
-                    player.teleport(plugin.getLocationManager().getPosition("spawn").getLocation());
-                    player.sendMessage(SkyMythPlugin.PREFIX + "§7Du wurdest zum §eSpawn §7teleportiert.");
-                    player.playSound(player.getLocation(), Sound.ENDERDRAGON_WINGS, 1.0f, 1.0f);
-                    this.cancel();
-                }
-            }
-        };
-
-        task.runTaskTimer(plugin, 0L, 1L);
-        teleportTasks.put(player, task);
+            task.runTaskTimer(plugin, 0L, 1L);
+            teleportTasks.put(player, task);
+        } else {
+            player.teleport(plugin.getLocationManager().getPosition("spawn").getLocation());
+            TitleUtil.sendTitle(player, 0, 40, 20, "§a§lSpawn", "§8× §7Du wurdest teleportiert §8×");
+            player.playSound(player.getLocation(), Sound.ENDERDRAGON_WINGS, 1.0f, 1.0f);
+        }
     }
 }
