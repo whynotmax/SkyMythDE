1541c58233521d5e2ab25f45e7a98f0fafc9096a
KeineSecrets <miethe.max@web.de>
2024-11-16 12:38:36 +0100

    Added AdminBadgeCommand

--- src/main/java/de/skymyth/badge/BadgeManager.java
+++ src/main/java/de/skymyth/badge/BadgeManager.java
@@ -3,12 +3,15 @@ package de.skymyth.badge;
 import de.skymyth.SkyMythPlugin;
 import de.skymyth.badge.model.Badge;
 import de.skymyth.badge.repository.BadgeRepository;
+import eu.decentsoftware.holograms.api.utils.scheduler.S;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Getter
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
@@ -17,31 +20,42 @@ public class BadgeManager {
     SkyMythPlugin plugin;
     BadgeRepository badgeRepository;
 
-    List<Badge> cachedBadges;
+    Map<String, Badge> cachedBadges;
 
     public BadgeManager(SkyMythPlugin plugin) {
         this.plugin = plugin;
         this.badgeRepository = plugin.getMongoManager().create(BadgeRepository.class);
 
-        this.cachedBadges = new ArrayList<>(badgeRepository.findAll());
+        this.cachedBadges = badgeRepository.findAll().stream().collect(Collectors.toMap(Badge::getName, badge -> badge));
     }
 
     public void createBadge(Badge badge) {
+        if (cachedBadges.values().stream().anyMatch(b -> b.getName().equalsIgnoreCase(badge.getName()))) {
+            throw new IllegalArgumentException("Badge with name " + badge.getName() + " already exists.");
+        }
         badgeRepository.save(badge);
-        cachedBadges.add(badge);
+        cachedBadges.put(badge.getName(), badge);
     }
 
     public void deleteBadge(Badge badge) {
+        if (!cachedBadges.containsValue(badge)) {
+            throw new IllegalArgumentException("Badge with name " + badge.getName() + " does not exist.");
+        }
         badgeRepository.delete(badge);
-        cachedBadges.remove(badge);
+        cachedBadges.remove(badge.getName());
+    }
+
+    public void saveBadge(Badge badge) {
+        cachedBadges.put(badge.getName(), badge);
+        badgeRepository.save(badge);
     }
 
     public Badge getBadge(String name) {
-        return cachedBadges.stream().filter(badge -> badge.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+        return cachedBadges.values().stream().filter(badge -> badge.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
     }
 
     public List<Badge> getBadges(UUID user) {
-        return cachedBadges.stream().filter(badge -> badge.getOwners().contains(user)).toList();
+        return cachedBadges.values().stream().filter(badge -> badge.getOwners().contains(user)).toList();
     }
 
 }
---
+++ src/main/java/de/skymyth/commands/impl/AdminBadgeCommand.java
@@ -0,0 +1,108 @@
+package de.skymyth.commands.impl;
+
+import de.skymyth.SkyMythPlugin;
+import de.skymyth.badge.model.Badge;
+import de.skymyth.commands.MythCommand;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminBadgeCommand extends MythCommand {
+
+    public AdminBadgeCommand(SkyMythPlugin plugin) {
+        super("adminbadge", "myth.owner", List.of("abadge", "badgeadmin"), plugin);
+    }
+
+    public void sendHelp(Player player) {
+        player.sendMessage(SkyMythPlugin.PREFIX + "§7Verwende:");
+        player.sendMessage(SkyMythPlugin.PREFIX + "§7/abadge create <name> <char> <description>");
+        player.sendMessage(SkyMythPlugin.PREFIX + "§7/abadge delete <name>");
+        player.sendMessage(SkyMythPlugin.PREFIX + "§7/abadge add <player> <badge>");
+        player.sendMessage(SkyMythPlugin.PREFIX + "§7/abadge remove <player> <badge>");
+    }
+
+    @Override
+    public void run(Player player, String[] args) {
+        if (args.length == 0) {
+            this.sendHelp(player);
+            return;
+        }
+        switch (args[0].toLowerCase()) {
+            case "create":
+                if (args.length < 4) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§7Verwende: /abadge create <name> <char> <description>");
+                    return;
+                }
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 3; i < args.length; i++) {
+                    stringBuilder.append(args[i].replace('&', '§')).append(" ");
+                }
+                plugin.getBadgeManager().createBadge(new Badge(
+                        args[1],
+                        stringBuilder.toString().trim(),
+                        String.valueOf(args[2].charAt(0)),
+                        System.currentTimeMillis(),
+                        player.getUniqueId(),
+                        new ArrayList<>()
+                ));
+                player.sendMessage(SkyMythPlugin.PREFIX + "§7Badge §e" + args[1] + " §7erstellt.");
+                break;
+            case "delete":
+                if (args.length < 2) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§7Verwende: /abadge delete <name>");
+                    return;
+                }
+                Badge badge = plugin.getBadgeManager().getBadge(args[1]);
+                if (badge == null) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§cDieses Badge existiert nicht.");
+                    return;
+                }
+                plugin.getBadgeManager().deleteBadge(badge);
+                player.sendMessage(SkyMythPlugin.PREFIX + "§7Badge §e" + args[1] + " §7gelöscht.");
+                break;
+            case "add":
+                if (args.length < 3) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§7Verwende: /abadge add <player> <badge>");
+                    return;
+                }
+                Badge badge1 = plugin.getBadgeManager().getBadge(args[2]);
+                if (badge1 == null) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§cDieses Badge existiert nicht.");
+                    return;
+                }
+                OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(args[1]);
+                if (offlinePlayer == null) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§cDieser Spieler existiert nicht.");
+                    return;
+                }
+                badge1.getOwners().add(offlinePlayer.getUniqueId());
+                plugin.getBadgeManager().saveBadge(badge1);
+                player.sendMessage(SkyMythPlugin.PREFIX + "§7Badge §e" + args[2] + " §7zu §e" + offlinePlayer.getName() + "§7 hinzugefügt.");
+                break;
+            case "remove":
+                if (args.length < 3) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§7Verwende: /abadge remove <player> <badge>");
+                    return;
+                }
+                Badge badge2 = plugin.getBadgeManager().getBadge(args[2]);
+                if (badge2 == null) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§cDieses Badge existiert nicht.");
+                    return;
+                }
+                OfflinePlayer offlinePlayer1 = plugin.getServer().getOfflinePlayer(args[1]);
+                if (offlinePlayer1 == null) {
+                    player.sendMessage(SkyMythPlugin.PREFIX + "§cDieser Spieler existiert nicht.");
+                    return;
+                }
+                badge2.getOwners().remove(offlinePlayer1.getUniqueId());
+                plugin.getBadgeManager().saveBadge(badge2);
+                player.sendMessage(SkyMythPlugin.PREFIX + "§7Badge §e" + args[2] + " §7von §e" + offlinePlayer1.getName() + "§7 entfernt.");
+                break;
+            default:
+                this.sendHelp(player);
+                break;
+        }
+    }
+}
