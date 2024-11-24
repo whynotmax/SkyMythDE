ef19a7027f28539ed2305dfce74244e99926a049
Keine Secrets <miethe.max@web.de>
2024-11-24 22:49:06 +0100

    Added Killstreak

--- src/main/java/de/skymyth/user/model/User.java
+++ src/main/java/de/skymyth/user/model/User.java
@@ -77,6 +77,7 @@ public class User {
         this.pvpShards -= amount;
     }
 
+    @Transient
     public void addKill() {
         this.currentKillStreak++;
         if (this.currentKillStreak >= this.biggestKillStreak) {
@@ -90,6 +91,7 @@ public class User {
         this.kills++;
     }
 
+    @Transient
     public void addDeath() {
         this.deaths++;
         if (this.killstreak) {
